3f0d16062d7dfc32a2b389b00cdd5b7c54eb0dde
Ryuji <giants0526@icloud.com>
2021-06-20 13:38:27 +0900

    [追加]バックとフロントの連携 #5

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  namespace :api, {format: "json"} do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
     end
   end
 end
--- backend/db/migrate/20210618045258_devise_token_auth_create_users.rb
+++ backend/db/migrate/20210618045258_devise_token_auth_create_users.rb
@@ -16,11 +16,12 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## User Info
       t.string :name
@@ -37,6 +38,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
   end
 end
