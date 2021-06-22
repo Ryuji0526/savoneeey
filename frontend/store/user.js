8dde6f52934e2bb54f3fb605a3d9956b07939689
Ryuji <giants0526@icloud.com>
2021-06-23 08:39:40 +0900

    [修正]本番環境に対応 #10

--- frontend/store/user.js
+++ frontend/store/user.js
@@ -1,11 +1,11 @@
 export const actions = {
   signUp({ commit }, Data) {
-    console.log('Data' + Data)
+    console.log(Data)
     this.$axios
       .post('/api/v1/auth', Data)
       .then((res) => {
         console.log('新規登録成功')
-        console.log(this.$auth.user)
+        console.log(res)
         this.$auth
           .loginWith('local', {
             data: {
@@ -15,6 +15,7 @@ export const actions = {
           })
           .then((res) => {
             console.log('登録&&ログイン成功')
+            console.log(this.$auth.user)
             console.log(res)
             this.$router.push('/')
             return res
