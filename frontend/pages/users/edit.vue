5dbece6ac69d88a81c04cedb9cf90feb8c4b7db6
Ryuji <giants0526@icloud.com>
2021-06-26 14:32:50 +0900

    [追加]ユーザー情報編集機能の追加 #19

--- frontend/pages/users/edit.vue
+++ frontend/pages/users/edit.vue
@@ -1,68 +1,18 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-card width="400px" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">メールアドレス変更</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" lazy-validation>
-            <v-text-field
-              v-model="user.name"
-              prepend-icon="mdi-email"
-              label="名前"
-            />
-            <v-text-field
-              v-model="user.email"
-              prepend-icon="mdi-email"
-              label="メールアドレス"
-            />
-            <v-text-field
-              v-model="user.password"
-              prepend-icon="mdi-lock"
-              append-icon="mdi-eye-off"
-              label="パスワード"
-            />
-            <v-card-actions>
-              <v-btn
-                color="light-green darken-1"
-                class="white--text"
-                @click="editEmail"
-              >
-                保存する
-              </v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-container>
-  </v-app>
+  <v-container>
+    <message />
+    <edit-user />
+  </v-container>
 </template>
 
 <script>
+import Message from '~/components/Message'
+import EditUser from '~/components/EditUser'
+
 export default {
-  name: 'App',
-  data() {
-    return {
-      user: {
-        name: this.$auth.user.name,
-        email: this.$auth.user.email,
-        password: '',
-      },
-    }
-  },
-  methods: {
-    editEmail() {
-      this.$axios
-        .put('api/v1/auth', this.user)
-        .then((res) => {
-          console.log(res)
-          this.$router.push('/')
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
+  compontents: {
+    Message,
+    EditUser,
   },
 }
 </script>
