ac9f3e67b18493a2cb42450197eceb12adbb744a
Ryuji <giants0526@icloud.com>
2021-06-26 13:21:58 +0900

    [追加, 修正]ログイン機能の追加と新規ユーザー登録機能の修正 #19

--- frontend/store/user.js
+++ frontend/store/user.js
@@ -22,11 +22,59 @@ export const actions = {
               }
             )
             this.$router.push('/')
-            return res
+            console.log(res)
           })
+          .catch((error) => {
+            console.log(error)
+          })
+      })
+      .catch((error) => {
+        dispatch(
+          'message/showFlashMessage',
+          {
+            content: '新規登録に失敗しました。もう一度登録をお願いします。',
+            type: 'error',
+          },
+          {
+            root: true,
+          }
+        )
+        console.log(error)
+      })
+  },
+  login({ dispatch }, Data) {
+    this.$auth
+      .loginWith('local', {
+        data: {
+          email: Data.email,
+          password: Data.password,
+        },
+      })
+      .then((res) => {
+        dispatch(
+          'message/showFlashMessage',
+          {
+            content: 'ログインしました。',
+            type: 'success',
+          },
+          {
+            root: true,
+          }
+        )
+        this.$router.push('/')
         return res
       })
       .catch((error) => {
+        dispatch(
+          'message/showFlashMessage',
+          {
+            content: 'メールアドレス、またはパスワードが違います。',
+            type: 'error',
+          },
+          {
+            root: true,
+          }
+        )
         console.log(error)
       })
   },
