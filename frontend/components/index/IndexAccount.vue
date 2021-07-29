c22ee31442489de0234ee7c84954a4cb74688414
Ryuji <giants0526@icloud.com>
2021-07-29 13:58:27 +0900

    [修正]Topページの画像の変更 #50

--- frontend/components/index/IndexAccount.vue
+++ frontend/components/index/IndexAccount.vue
@@ -2,14 +2,29 @@
   <v-container class="parallax scrollbox px-sm-15 px-7">
     <h1 class="text-center mb-15 text-h3 caption">\ Account</h1>
     <div class="width-1100">
-      <v-row>
-        <v-col cols="12" sm="6" order-md="last">aaa</v-col>
-        <v-col cols="12" sm="6">
+      <v-row align="center" justify="space-between">
+        <v-col cols="12" md="6" order-md="last">
+          <v-img
+            :src="require('~/assets/image/top-account.jpeg')"
+            aspect-ratio="1.4"
+            class="mx-auto"
+          >
+            <template #placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminatef
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-col>
+        <v-col cols="12" sm="5">
           <h3 class="text-h6 font-weight-bold">
             夢ごとに口座を分けて貯蓄をスタート
           </h3>
           <p class="text-body-2 mt-7">
-            各項目の残高を「見える化」することで、使いすを防ぐことができたり、目標達成までどれくらい頑張ればいいのかが見えるようになります。「あとこれだけの金額しか残っていないから、今月はもう使いすぎないようにセーブしよう…」や、「今月は余裕があるから少し貯金を増やそう！」などを「ACCOUNT」で調節することができます。
+            各項目の残高を「見える化」することで、使いすを防ぐことができたり、目標達成までどれくらい頑張ればいいのかが見えるようになります。「あとこれだけの金額しか残っていないから、今月はもう使いすぎないようにセーブしよう…」や、「今月は余裕があるから少し貯金を増やそう！」などを「Account」で調節することができます。
           </p>
           <v-btn
             outlined
