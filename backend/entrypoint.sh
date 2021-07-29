ac7b9869f35380319533826b08413e159b5549eb
Ryuji <giants0526@icloud.com>
2021-07-30 00:52:47 +0900

    [修正]seedの修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,6 @@
 sudo service nginx start
 cd /app
 bin/setup
-bundle exec rails db:migrate:reset
+bundle exec rails db:migrate:reset RAILS_ENV=production
 bundle exec rails db:seed
 bundle exec pumactl start
