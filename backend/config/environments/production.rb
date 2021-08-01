3817245f1cd1d8813d12ee27fb12065e4b1824d4
Ryuji <giants0526@icloud.com>
2021-08-02 01:30:03 +0900

    [修正]AWS SESからメールを送信 #54

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   
-  config.action_mailer.default_url_options = { host: ENV['APP_DOMAIN'] }
+  config.action_mailer.default_url_options = { host: ENV['BACK_DOMAIN'] }
   config.action_mailer.delivery_method = :ses
 
   # Ignore bad email addresses and do not raise email delivery errors.
